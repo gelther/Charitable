d17eb235738e52a499ec96893e3a2a1e06a27827
JeroenSormani <jeroen.sormani@gmail.com>
2017-07-03 09:09:10 +0200

    Code consistency changes for includes/class-charitable-request.php

--- includes/class-charitable-request.php
+++ includes/class-charitable-request.php
@@ -94,8 +94,8 @@ if ( ! class_exists( 'Charitable_Request' ) ) :
 		/**
 		 * When the_post is set, sets the current campaign to the current post if it is a campaign.
 		 *
-		 * @param 	WP_Post $post The Post object.
-		 * @return 	void
+		 * @param   WP_Post  $post  The Post object.
+		 * @return  void
 		 * @access  public
 		 * @since 	1.0.0
 		 */
@@ -114,7 +114,7 @@ if ( ! class_exists( 'Charitable_Request' ) ) :
 		/**
 		 * Returns the current campaign. If there is no current campaign, return false.
 		 *
-		 * @return 	Charitable_Campaign|false Campaign object if we're viewing a campaign within a loop. False otherwise.
+		 * @return  Charitable_Campaign|false  Campaign object if we're viewing a campaign within a loop. False otherwise.
 		 * @access 	public
 		 * @since 	1.0.0
 		 */
@@ -138,7 +138,7 @@ if ( ! class_exists( 'Charitable_Request' ) ) :
 		/**
 		 * Returns the current campaign ID. If there is no current campaign, return 0.
 		 *
-		 * @return 	int
+		 * @return  int
 		 * @access  public
 		 * @since 	1.0.0
 		 */
@@ -231,6 +231,7 @@ if ( ! class_exists( 'Charitable_Request' ) ) :
 
 			return $donation_id;
 		}
+
 	}
 
 endif;
